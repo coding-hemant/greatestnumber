0d86563d4d97ac6297b3fb3ec1588e2a96dccd21
coding-hemant <104069141+coding-hemant@users.noreply.github.com>
2022-04-21 16:32:12 +0530

    Add files via upload

---
+++ day2.java
@@ -0,0 +1,26 @@
+package assignment2;
+
+public class day2 
+{
+	
+	public static void main(String[]args)
+	{
+		int x=10,y=20,z=15;
+		//System.out.println("the three numbers are");
+		if(x>y && x>z)
+		{
+			System.out.println("the greatest number is:"+x);	
+		}
+		else if(y>x && y>z)
+		{
+			System.out.println("the greatest number is:"+y);
+		
+	}
+	else
+	{
+		System.out.println("the greatest number is:"+z);
+		
+	}
+
+  }
+}
